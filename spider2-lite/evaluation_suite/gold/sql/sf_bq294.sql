84911f375c87903c73ed5fe93769b593a14efb42
lfy79001 <843265183@qq.com>
2025-08-06 12:01:15 +0800

    fix sf_bq294 eval

--- spider2-lite/evaluation_suite/gold/sql/sf_bq294.sql
+++ spider2-lite/evaluation_suite/gold/sql/sf_bq294.sql
@@ -1,7 +1,7 @@
 SELECT
   "trip_id",
   "duration_sec",
-  DATE(TO_TIMESTAMP_LTZ("start_date" / 1000000)) AS "star_date", -- 将微秒转换为日期
+  DATE(TO_TIMESTAMP_LTZ("start_date" / 1000000)) AS "star_date",
   "start_station_name",
   CONCAT("start_station_name", ' - ', "end_station_name") AS "route",
   "bike_number",
@@ -17,11 +17,11 @@ SELECT
   c."name" AS "region_name"
 FROM "SAN_FRANCISCO_PLUS"."SAN_FRANCISCO_BIKESHARE"."BIKESHARE_TRIPS" a
 LEFT JOIN "SAN_FRANCISCO_PLUS"."SAN_FRANCISCO_BIKESHARE"."BIKESHARE_STATION_INFO" b 
-  ON a."start_station_name" = b."name"
+  ON a."start_station_id" = b."station_id"
 LEFT JOIN "SAN_FRANCISCO_PLUS"."SAN_FRANCISCO_BIKESHARE"."BIKESHARE_REGIONS" c 
   ON b."region_id" = c."region_id"
 WHERE TO_TIMESTAMP_LTZ("start_date" / 1000000) BETWEEN '2017-07-01' AND '2017-12-31'
-  AND b."name" IS NOT NULL
+  AND b."station_id" IS NOT NULL
   AND "member_birth_year" IS NOT NULL
   AND "member_gender" IS NOT NULL
 ORDER BY "duration_sec" DESC
